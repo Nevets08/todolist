05b7af2c61add0ee2d9e271c77b489449bbc4703
Nevets08 <Nevets08@users.noreply.github.com>
2020-06-23 22:41:49 +0200

    Edit the list

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const filtersCounter = document.querySelector('.filters p span');
 const filters = document.querySelector('.filters');
 let done = false;
 let todoCount = 0;
-
 // edit
 // console.log(list.lastChild.children[2])
 // delete
@@ -12,52 +11,49 @@ let todoCount = 0;
 list.children[1].addEventListener('keydown', (e) => {
     // Create div .list contains an input checkbox, the value of the input and icons : delete and edit
     if (e.key === 'Enter') {
-        // Doesn't create divs when there is only spaces
-        // if (list.children[1].value == '') {
-        //     return;
-        // }
-
+        
+        
+        // TODO : Doesn't create divs when there is only spaces
+        
         const listAdded = document.createElement('div');
-        listAdded.innerHTML = "<input type='checkbox'>" + '<span id="listContent" contenteditable="true">' + list.children[1].value + '</span>' + '<i class="fas fa-edit"></i>' + '<i class="fas fa-trash"></i>';
+        listAdded.innerHTML = "<input type='checkbox'>" + "<input type='text' id='listContent' value=" + list.children[1].value + ">" + '<i class="fas fa-trash"></i>';
         listAdded.classList.add('list');
         list.appendChild(listAdded);
-
+        
         // Clear the value of input text
         list.children[1].value = '';
-
+        
         // Count the task left when it is created
         todoCount++;
         filtersCounter.innerHTML = todoCount;
-
+        
+        const contentListInput = document.getElementById('listContent');
         // When the task is done : click on the checkbox
-        listAdded.addEventListener('change', () => {
-            if (listAdded.classList.contains('strikethrough')) {
-                listAdded.classList.remove('strikethrough');
-
-                done = false;
-
+        list.lastChild.firstChild.addEventListener('change', () => {
+            if (contentListInput.classList.contains('strikethrough')) {
+                contentListInput.classList.remove('strikethrough');
+                
                 todoCount++;
                 filtersCounter.innerHTML = todoCount;
             } else {
-                listAdded.classList.add('strikethrough');
-
-                done = true;
-
+                contentListInput.classList.add('strikethrough');
+                        
                 todoCount--;
                 filtersCounter.innerHTML = todoCount;
             }
         });
-
+        
         const icons = document.querySelectorAll('.container-list i');
-
         // Buttons on list created (edit, delete)
         icons.forEach(icon => {
             icon.addEventListener('click', () => {
                 // Delete button
                 if (icon.className === 'fas fa-trash') {
                     icon.parentElement.remove();
+                    todoCount--;
+                    filtersCounter.innerHTML = todoCount;
                 }
-            });
+            });           
         });
     };
 });
